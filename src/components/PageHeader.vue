78a05345ae8bc138127babdcaad8fb72f5286ba7
zhengze00 <zhengze00@hotmail.com>
2024-07-13 22:04:35 +0900

    Vue+SpringBoot

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<!-- PageHeader.vue -->
+<template>
+  <header>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/board/list">Board</router-link>
+    </div>
+  </header>
+  <hr/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from '@/views/PageHome.vue'
+import BoardList from '@/views/board/BoardList.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'PageHome',
+        component: PageHome
+    },
+    {
+        path: '/about',
+        name: 'PageAbout',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
+    },
+    {
+        path: '/board/list',
+        name: 'BoardList',
+        component: BoardList
+    }
+
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
